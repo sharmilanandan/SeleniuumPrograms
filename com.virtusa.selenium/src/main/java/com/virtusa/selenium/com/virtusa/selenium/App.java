ae01aa1fc18c56c8d33d58bb16667414de00973f
sharmilanandan <devi.sharmila37@gmail.com>
2018-08-01 05:12:06 +0530

    first commit

---
+++ com.virtusa.selenium/src/main/java/com/virtusa/selenium/com/virtusa/selenium/App.java
@@ -0,0 +1,13 @@
+package com.virtusa.selenium.com.virtusa.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
